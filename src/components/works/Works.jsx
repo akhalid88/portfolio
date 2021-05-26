2c2f7ba85322382a4ba41e3b6a42bf150719ef94
Muhammad K <akhalid.code@gmail.com>
2021-05-26 13:25:41 -0700

    slider + animation

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import "./works.scss";
 
 export default function Works() {
+
+	const [currentSlide, setCurrentSlide] = useState(0);
 
 	const data = [
 		{
@@ -32,9 +35,15 @@ export default function Works() {
 		},
 	];
 
+	const handleClick = (way) => {
+		way === "left"
+			? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+			: setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+	}
+
 	return (
 		<div className="works" id="works">
-			<div className="slider">
+			<div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
 				{data.map((d) => (
 					<div className="container">
 						<div className="item">
@@ -54,8 +63,8 @@ export default function Works() {
 						</div>
 					</div>))}
 			</div>
-			<img src="assets/arrow.png" className="arrow left" alt="" />
-			<img src="assets/arrow.png" className="arrow right" alt="" />
+			<img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClick("left")} />
+			<img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClick()} />
 		</div>
 	)
 }
