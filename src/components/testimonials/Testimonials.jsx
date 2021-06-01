1b71ea1b0aeaf4e02ef6442db7820be6c1a92639
Muhammad K <akhalid.code@gmail.com>
2021-06-01 11:30:13 -0700

    testimonial section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,23 @@ import "./testimonials.scss";
 export default function Testimonials() {
 	return (
 		<div className="testimonials" id="testimonials">
-			{/* testimonials */}
+			<h1>Testimonials</h1>
+			<div className="container">
+				<div className="card">
+					<div className="top">
+						<img src="assets/right-arrow.png" className="left" alt="" />
+						<img src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg" className="user" alt="" />
+						<img src="assets/youtube.png" className="right" alt="" />
+					</div>
+					<div className="center">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quasi eaque odio, magni repellat et mollitia ea maiores cumque neque libero?
+					</div>
+					<div className="bottom">
+						<h3>Alex</h3>
+						<h4>CEO of LAMA</h4>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
